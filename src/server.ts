e14ab4225dfc1baa6078b855549b6c349a93018f
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-09 18:39:59 -0500

    feat: implement middlewares for cors, json and parsing url-encoded payload

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+// Middlewares
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Routes
+app.route("/api").get((req: Request, res: Response) => {
+  res.send({
+    message: "Hello World!",
+  });
 });
+
+// 404 Page
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" })
+})
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
